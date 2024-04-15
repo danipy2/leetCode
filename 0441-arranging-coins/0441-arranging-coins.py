51e13a7f2067b900a22e3c82d8699efc3fa546ea
danipy2 <165833791+danipy2@users.noreply.github.com>
2024-04-15 23:15:13 +0300

    Time: 41 ms (66.54%), Space: 16.6 MB (40.75%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -5,7 +5,6 @@ class Solution:
         while (left <= right):
             mid = (left + right) // 2
             if (((mid) * (mid + 1)) / 2)<=n and ((mid + 2) * (mid + 1) / 2)>n:
-                print(mid,3)
                 return mid
             if ((mid + 1) * mid) / 2 <= n:
                 left = mid + 1
