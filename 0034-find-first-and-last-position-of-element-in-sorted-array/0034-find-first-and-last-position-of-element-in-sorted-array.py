f5b1f680489dddf3f8dd2d28b12dc0cfb9c1c6f9
danipy2 <165833791+danipy2@users.noreply.github.com>
2024-04-19 23:56:22 +0300

    Time: 76 ms (56.09%), Space: 17.8 MB (84.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -7,26 +7,22 @@ class Solution:
             mid = (left+right) //2
             if nums[mid] == target and (mid == 0 or nums[mid-1] != target ):
                 arr.append(mid)
-                break
+                if mid < len(nums)-1:
+                    if nums[mid+1] == target:
+                        while(mid<len(nums)-1 and nums[mid+1]==target):
+                            mid +=1
+                        arr.append(mid)
+                        return arr
+                    else:
+                        arr.append(arr[0])
+                    return arr
+                else:
+                    arr.append(arr[0])
+                    return arr
             elif nums[mid] >=target:
                 right = mid-1
             else:
                 left = mid + 1
-        if len(arr) == 0:
-            return [-1,-1]
-        left = 0
-        right = len(nums)-1
-        while(left<=right):
-            mid = (left+right) //2
-            if nums[mid] == target and (mid == len(nums)-1 or nums[mid+1] != target  ):
-                arr.append(mid)
-                break
-            elif nums[mid] <= target:
-                left = mid + 1
-            else:
-                right = mid-1
-        if len(arr) == 1:
-            arr.append(arr[0])
-        return arr
+        return [-1,-1]
         
             
