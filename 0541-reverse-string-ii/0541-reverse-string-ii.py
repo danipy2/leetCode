aae5bb1ae85736d9709c00f9018683d4343ba637
danipy2 <165833791+danipy2@users.noreply.github.com>
2024-05-02 22:00:28 +0300

    Time: 40 ms (35.70%), Space: 16.7 MB (49.50%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         left = 0
-        s = [i for i in s]
         if k > len(s):
             k = len(s)
         i = k-1
@@ -9,10 +8,9 @@ class Solution:
 
         while(i<len(s)):
             y = i
-            while(left<i):
-                s[i],s[left] = s[left],s[i]
-                left += 1
-                i -= 1
+            tempo = s[left:i+1]
+            tempo = tempo[::-1]
+            s = s[:left] + tempo + s[i+1:]
             if y + 2*k >= len(s) and len(s)-1-y >=k:
                 i = len(s)-1
             else:
@@ -22,4 +20,4 @@ class Solution:
             x += 2*k
             left = x
             
-        return "".join(s)
+        return s
