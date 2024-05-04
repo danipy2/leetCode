4d8c28ce1f15b2f1285a8d03f4e2aa229aff268a
danipy2 <165833791+danipy2@users.noreply.github.com>
2024-05-04 21:02:04 +0300

    Time: 36 ms (91.44%), Space: 16.7 MB (16.72%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -7,10 +7,13 @@ class Solution:
             if s[i] == c:
                 places.append(i)
         for i in range(len(s)):
-                if i >places[left] and left+1 <len(places):
+                if s[i] == c:
+                    arr.append(0)
                     left += 1
-                if left>=len(places):
-                    break
+                elif i<places[0]:
+                    arr.append(places[0]-i)
+                elif i>places[-1]:
+                    arr.append(i-places[-1])
                 else:
-                    arr.append(min(abs(i-places[left-1]),abs(i-places[left])))
+                    arr.append(min(i-places[left-1],places[left]-i))
         return arr
