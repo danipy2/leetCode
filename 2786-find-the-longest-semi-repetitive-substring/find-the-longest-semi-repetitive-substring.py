415e19acd54f1fdaedd1cbc11bae164aff127e2f
Daniel mola <165833791+danipy2@users.noreply.github.com>
2024-10-20 11:32:51 +0300

    Time: 10 ms (100.00%) | Memory: 16.7 MB (14.39%) - LeetSync

--- 2786-find-the-longest-semi-repetitive-substring/find-the-longest-semi-repetitive-substring.py
+++ 2786-find-the-longest-semi-repetitive-substring/find-the-longest-semi-repetitive-substring.py
@@ -1,24 +1,14 @@
 class Solution:
     def longestSemiRepetitiveSubstring(self, s: str) -> int:
         count = 0
-        maxm = 0
-        found = False
-        left = 0
+        maxm = 1
+        l = 0
         for i in range(1,len(s)):
             if s[i] ==s[i-1]:
                 count+=1
-            if count==2:
-                if i-left > maxm:
-                    maxm = i-left
-                    found = True
-                count = 1
-                left = left+1
-                while left<=i and (s[left]!=s[left-1]):
-                    left = left+1
-            elif i == len(s)-1:
-                if (i-left)+1>maxm:
-                    maxm = (i-left)+1
-
-        if found ==0:
-            maxm = len(s)
+            while(l+1<i) and count>1:
+                if s[l] == s[l+1]:
+                    count-=1
+                l+=1
+            maxm = max(maxm,i-l+1)
         return maxm
