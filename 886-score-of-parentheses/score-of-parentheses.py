c32bd43070d70df3eb38a7a924d4b737f0e88f3d
Daniel mola <165833791+danipy2@users.noreply.github.com>
2024-10-24 17:19:08 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (7.81%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.py
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -2,12 +2,11 @@ class Solution:
     def scoreOfParentheses(self, s: str) -> int:
         contain = ""
         ss = ""
-        output = 0
         for i in s:
             if i=="(":
                 ss+="+("
             else:
-                if contain and contain=="(":
+                if contain=="(":
                     ss+="1)"
                 else:
                     ss+=")*2"
