581d2801ce55525c04e1a378cd789c8e13e2a38b
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-02-20 21:06:50 +0300

    Time: 1202 ms (5.15%) | Memory: 34.9 MB (91.82%) - LeetSync

--- 456-132-pattern/132-pattern.py
+++ 456-132-pattern/132-pattern.py
@@ -2,8 +2,13 @@ class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         stack = [[nums[0],nums[0]]]
         minm = nums[0]
+        maxm = -float('inf')
         for i in range(1,len(nums)):
+            if nums[i] >maxm:
+                stack = [[nums[i],minm]]
+                maxm  = nums[i]
             while stack and stack[-1][0] <= nums[i]:
+                
                 stack.pop()
             else:
                 if i > 1:
