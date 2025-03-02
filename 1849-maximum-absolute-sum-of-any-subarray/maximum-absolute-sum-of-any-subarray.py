d143d7e11349b7b576837ba3da88b1caf9e4e81d
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-02 16:40:58 +0300

    Time: 50 ms (72.86%) | Memory: 28.5 MB (60.93%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -1,20 +1,14 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
-        p = [0]
-        for i in nums:
-            p.append(p[-1]+i)
-        l = 0
-        k = 0
-        maxm = p[1]
-        minm = maxm
-        print(p)
-        for i in range(len(p)):
-            if p[i] < p[l]:
-                l=i
-            if p[i] > p[k]:
-                k=i
-                
-            maxm = max(maxm,p[i]-p[l])
-            minm = min(minm,p[i]-p[k])
-            print(k,minm)
-        return max(abs(minm),maxm)
+        pref = 0
+        min_pref = 0
+        max_pref = 0
+        
+        # Iterate through the array and update the prefix sum and its min/max.
+        for num in nums:
+            pref += num
+            min_pref = min(min_pref, pref)
+            max_pref = max(max_pref, pref)
+        
+        # The maximum absolute sum is the difference between the maximum and minimum prefix sum.
+        return max_pref - min_pref
