10c015f760693e827da6de574dac79f447ea88ea
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-27 15:57:02 +0300

    Time: 257 ms (13.91%) | Memory: 29.4 MB (96.89%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,23 +1,26 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-
-        for i in range(1,len(matrix)):
-            matrix[i][0] += matrix[i-1][0]
-        for i in range(1,len(matrix[0])):
-            matrix[0][i] += matrix[0][i-1]
-        for i in range(1,len(matrix)):
-            for j in range(1,len(matrix[0])):
-                matrix[i][j]+= matrix[i][j-1]+matrix[i-1][j]-matrix[i-1][j-1]
         self.matrix = matrix
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i > 0:
+                    matrix[i][j] += matrix[i-1][j]
+                if j > 0:
+                    matrix[i][j] += matrix[i][j-1]
+                if i > 0 and j > 0:
+                    matrix[i][j] -= matrix[i-1][j-1]
+
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        total = self.matrix[row2][col2]
-        if row1  and col1:
-            total += -self.matrix[row1-1][col2] - self.matrix[row2][col1-1]+self.matrix[row1-1][col1-1]
-        elif row1:
-            total += -self.matrix[row1-1][col2]
-        elif col1:
-            total += -self.matrix[row2][col1-1]
+        total =  self.matrix[row2][col2] 
+        print(total,row2,col2)
+        if row1:
+            total-= self.matrix[row1-1][col2]
+        if col1:
+            total-= self.matrix[row2][col1-1]
+        print(total)
+        if row1 and col1:
+            total += self.matrix[row1-1][col1-1]
         return total
 
 
