024eb40f834dd47455fd4a8aefb6088322c86765
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-27 15:57:11 +0300

    Time: 139 ms (29.76%) | Memory: 29.6 MB (94.09%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -13,12 +13,10 @@ class NumMatrix:
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         total =  self.matrix[row2][col2] 
-        print(total,row2,col2)
         if row1:
             total-= self.matrix[row1-1][col2]
         if col1:
             total-= self.matrix[row2][col1-1]
-        print(total)
         if row1 and col1:
             total += self.matrix[row1-1][col1-1]
         return total
