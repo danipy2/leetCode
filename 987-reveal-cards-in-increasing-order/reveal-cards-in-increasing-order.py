459a3bd6ab8b53b5416073dbe54e34889b79b3fb
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-29 09:29:39 +0300

    Time: 8 ms (5.43%) | Memory: 18.3 MB (9.20%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -1,17 +1,19 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         out = [0 for i in deck]
-        deck.sort()
-        curr = 0
-        out[0] = deck[0]
-        for i in deck[1:]:
-            steps = 1
-            while True:
-                if out[curr%len(out)]==0:
-                    if steps:
-                        steps-=1
-                    else:
-                        out[curr%len(out)] = i
-                        break
+        st = []
+        curr = -1
+        myset = set()
+        for i in range(len(deck)):
+            while curr%len(deck) in myset:
                 curr+=1
+            curr+=1
+            while curr%len(deck) in myset:
+                curr+=1
+
+            st.append(curr%len(deck))
+            myset.add(curr%len(deck))
+        deck.sort()
+        for i in range(len(deck)):
+            out[st[i]] = deck[i]
         return out
