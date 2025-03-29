a67e60871d287f69ab9ce32c402cf7108639392d
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-29 20:37:55 +0300

    Time: 79 ms (83.39%) | Memory: 17.8 MB (68.24%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        t = 0
+        arr = [-1,-1,-1]
+        l = 0
+        for i in range(len(s)):
+            arr[ord(s[i])-ord('a')] = i
+            if arr[0]>=l and arr[1]>=l and arr[2]>=l:
+                t +=  min(arr[0],arr[1],arr[2])+1
+        return t
