8625ba3153063cef93e08a9ae43a491b45b10481
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-29 21:22:13 +0300

    Time: 115 ms (79.01%) | Memory: 18.1 MB (23.55%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,12 +3,13 @@ class Solution:
         myd = {}
         l = 0
         total = 0
+        maxm = 1
         for i in range(len(s)):
             if s[i] in myd:
                 myd[s[i]] +=1
+                maxm = max(maxm,myd[s[i]])
             else:
                 myd[s[i]] =1
-            maxm = self.findmax(myd)
             if maxm + k >= i-l+1:
                 total = max(total,i-l+1)
             else:
@@ -18,9 +19,4 @@ class Solution:
                     myd[s[l]] -=1
                 l+=1
         return total
-    def findmax(self,arr):
-        maxm = 0
-        for i in arr:
-            maxm = max(maxm,arr[i])
-        return maxm
     
