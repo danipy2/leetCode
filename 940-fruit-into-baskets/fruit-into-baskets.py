ebaaf16a7a595dc9cfd9ba18f78af091a613bafe
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-29 14:14:23 +0300

    Time: 191 ms (44.27%) | Memory: 24.1 MB (7.97%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -3,16 +3,17 @@ class Solution:
         myd = {}
         l = 0
         maxm = 0
+        count = 0
         for i in range(len(fruits)):
-            if fruits[i] in myd:
+            if fruits[i] in myd and myd[fruits[i]]:
                 myd[fruits[i]]+=1
             else:
                 myd[fruits[i]]  = 1
-            if len(myd) >=3:
+                count+=1
+            if count >= 3:
                 if myd[fruits[l]] == 1:
-                    del  myd[fruits[l]]
-                else:
-                    myd[fruits[l]]-=1 
+                    count-=1
+                myd[fruits[l]]-=1 
                 l+=1 
             maxm = max(maxm,i-l+1)
         return maxm
