d60b59cf9a76a0ca5b5fbc04eb3cb7bb20eda5b9
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-29 09:43:20 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (95.45%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -3,17 +3,12 @@ class Solution:
         out = [0 for i in deck]
         st = []
         curr = -1
-        myset = set()
+        q = deque()
         for i in range(len(deck)):
-            while curr%len(deck) in myset:
-                curr+=1
-            curr+=1
-            while curr%len(deck) in myset:
-                curr+=1
-
-            st.append(curr%len(deck))
-            myset.add(curr%len(deck))
+            q.append(i)
         deck.sort()
         for i in range(len(deck)):
-            out[st[i]] = deck[i]
+            out[q.popleft()] = deck[i]
+            if q:
+                q.append(q.popleft())
         return out
