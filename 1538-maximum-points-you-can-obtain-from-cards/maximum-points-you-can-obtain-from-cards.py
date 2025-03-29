4f1373753ced6adfe1d8461aa9fa6699c4c218e0
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-29 12:02:26 +0300

    Time: 14 ms (98.53%) | Memory: 28.1 MB (44.63%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -1,20 +1,26 @@
 class Solution:
+
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        maxm = 0
-        total = 0
-        for i in range(k):
-            maxm += cardPoints[i]
-        if k==len(cardPoints):
-            return maxm
-        l = k-1
-        r = len(cardPoints)-1
-        total = maxm
 
-        while r>=len(cardPoints)-k:
-            total += cardPoints[r]-cardPoints[l]
-            maxm = max(maxm,total)
-            l-=1
-            r-=1
-        return maxm
+        if 2*k < len(cardPoints):
+            cardPoints = cardPoints[:k] + cardPoints[-k:] 
 
 
+
+        min_sum_len = len(cardPoints) - k
+        total_sum = sum(cardPoints)
+
+        if min_sum_len == 0:
+            return total_sum
+        
+        min_sum = total_sum
+        current_sum = sum(cardPoints[0:min_sum_len])
+        min_sum = min_sum if min_sum < current_sum else current_sum
+        to_sub = cardPoints[0]
+
+        for i in range(min_sum_len, len(cardPoints)):
+            current_sum = current_sum - to_sub + cardPoints[i]  
+            min_sum = min_sum if min_sum < current_sum else current_sum
+            to_sub = cardPoints[i - min_sum_len + 1]
+        
+        return total_sum - min_sum
