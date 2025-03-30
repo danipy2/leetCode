60a037e91d352f3b3cb9373ef1af38544a5720a1
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-30 21:22:45 +0300

    Time: 145 ms (28.54%) | Memory: 18.2 MB (78.13%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,13 +1,17 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if not t:
+            return ""
         myd = Counter(t)
         n = len(myd)
         l = 0
-        minm = s+"*"
+        minm = ""
+        size = len(s)+1
         for i in range(len(s)):
-            if s[i] in myd:
-                myd[s[i]] -=1
-                if myd[s[i]] == 0:
+            right_char = s[i]
+            if right_char in myd:
+                myd[right_char] -=1
+                if myd[right_char] == 0:
                     n-=1
             if l:
                 if s[l] in myd:
@@ -16,12 +20,13 @@ class Solution:
                         n+=1
                 l+=1
             while n==0:
-                if len(minm) > i-l+1:
+                if size > i-l+1:
                     minm = s[l:i+1]
+                    size = i-l+1
                 if s[l] in myd:
                     myd[s[l]] +=1
                     if myd[s[l]] ==1:
                         n+=1
                 l+=1
-        return minm if len(s) >= len(minm) else ""
+        return minm if len(s) >= size else ""
 
