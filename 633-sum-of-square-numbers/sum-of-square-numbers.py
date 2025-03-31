3937d35952d4fb3114dfdca41e415b1f7e32fc25
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-03-31 20:26:07 +0300

    Time: 127 ms (14.49%) | Memory: 21.3 MB (6.78%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,27 +1,13 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
+        sqr = 0
+        myset = set()
         n = 0
-        myd = {}
-        arr = []
-        if c<=1:
-            return True
-        while(n*n) <= c:
-            myd[n*n] = n
-            arr.append(n*n)
-            
-            n+=1
-        left = 0
-        right = len(arr)-1
-        while(left<=right):
-            if arr[left] +arr[right]==c:
+        while n <= c:
+            sqr = n*n
+            myset.add(sqr)
+            if sqr >c:
+                return False
+            if c-sqr in myset:
                 return True
-            elif arr[left] +arr[right] > c:
-                right-=1
-            else:
-                left+=1
-        return False
-
-        return False
-        
-
-        
+            n+=1
