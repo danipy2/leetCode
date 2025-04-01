621459649dc52c89b38c49b2e5cfb05a1e33de57
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-04-01 14:20:59 +0300

    Time: 60 ms (58.65%) | Memory: 17.8 MB (99.76%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -1,11 +1,11 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
         count = 0
-        l = 0
         total = 0
-        for i in range(len(s)):  
-            total += int(s[i])         
+        for i in range(len(s)):         
             if s[i] == "0":
                 count += total
+            else:
+                total+=1
             
         return count
