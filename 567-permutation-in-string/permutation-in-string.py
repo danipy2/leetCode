1287f107c13038d066782d95b076b386c74af9f4
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-04-01 11:08:04 +0300

    Time: 22 ms (55.71%) | Memory: 18.1 MB (19.59%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,37 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        dic = {}
-        myset = set()
-        if len(s1)>len(s2):
+        counter = Counter(s1)
+        myd = {}
+        if len(s1) > len(s2):
             return False
-        for i in s1:
-            if i in myset:
-                dic[i] +=1 
-            else:
-                myset.add(i)
-                dic[i] = 1
-        count=0
         for i in range(len(s1)):
-            if s2[i] in myset:
-                dic[s2[i]] -=1
-                if dic[s2[i]]==0:
-                    count+=1
-        if len(myset)==count:
+            myd[s2[i]] = myd.get(s2[i],0)+1
+        if  myd == counter:
             return True
-        left = 0
-        right=len(s1)-1
-
-        while(right+1<len(s2)):
-            right+=1
-            if s2[right] in myset:
-                dic[s2[right]] -=1
-                if dic[s2[right]]==0:
-                    count +=1
-            if s2[left] in myset:
-                if dic[s2[left]]==0:
-                    count-=1
-                dic[s2[left]] +=1
-            if count == len(myset):
+        l = 0
+        for i in range(len(s1),len(s2)):
+            myd[s2[l]] -=1
+            if myd[s2[l]] == 0:
+                del myd[s2[l]]
+            myd[s2[i]] = myd.get(s2[i],0)+1
+            l += 1
+            if myd == counter:
                 return True
-            left+=1
         return False
