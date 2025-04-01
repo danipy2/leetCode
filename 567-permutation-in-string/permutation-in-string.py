1262323c174d358cf1605bf6e7e5bbe5f0993445
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-04-01 11:14:08 +0300

    Time: 22 ms (55.71%) | Memory: 18 MB (19.59%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -4,17 +4,15 @@ class Solution:
         myd = {}
         if len(s1) > len(s2):
             return False
-        for i in range(len(s1)):
-            myd[s2[i]] = myd.get(s2[i],0)+1
-        if  myd == counter:
-            return True
         l = 0
-        for i in range(len(s1),len(s2)):
-            myd[s2[l]] -=1
-            if myd[s2[l]] == 0:
-                del myd[s2[l]]
+        for i in range(len(s2)):
             myd[s2[i]] = myd.get(s2[i],0)+1
-            l += 1
-            if myd == counter:
-                return True
+            if i >= len(s1)-1:
+                if myd == counter:
+                    return True
+                myd[s2[l]] -=1
+                if myd[s2[l]] == 0:
+                    del myd[s2[l]]
+                l += 1
+            
         return False
