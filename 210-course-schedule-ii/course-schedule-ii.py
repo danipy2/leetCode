6a555577ee09310fbc1f7f5621ec01ffea3ecbfb
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-05-09 19:18:46 +0300

    Time: 4 ms (49.96%) | Memory: 19.2 MB (60.15%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -6,17 +6,18 @@ class Solution:
             prereq[r].append(l) 
             req[l] += 1
         output = []
-        i = 0 
-        cond = True
-        while cond:
-            cond = False
-            for i in range(len(req)):
+        qeue = deque()
+        for i in range(len(req)):
+            if req[i] == 0:
+                output.append(i)
+                qeue.append(i)
+        while qeue:
+            n = qeue.popleft()
+            for i in prereq[n]:
+                req[i] -= 1
                 if req[i] == 0:
-                    req[i] = -1
+                    qeue.append(i)
                     output.append(i)
-                    for j in prereq[i]:
-                        req[j] -= 1
-                    cond = True
         if len(output) != numCourses:
             return []
         return output
