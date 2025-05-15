917b12c76afade1ad3af9b8dc32f71a70dcd2ff9
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-05-15 17:43:14 +0300

    Time: 19 ms (7.39%) | Memory: 18.7 MB (63.54%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -1,18 +1,19 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        
-        l = len(s)
-        arr = []
-        
+        l = 0
+        ind = []
+        for i in range(len(s)):
+            if s[i].isalpha():
+                ind.append(i)
+                l+=1
+        arr =[]
+        su = list(s)
         for i in range(1<<l):
-            sub = []
+            sub = su
             for j in range(l):
-                if i & (1<<j):
-                    if s[j].isalpha()==False:
-                        break
-                    sub.append(s[j].upper())
-                else:
-                    sub.append(s[j].lower())
-            if len(sub)==l:
-                arr.append("".join(sub))
+                    if i & (1<<j):
+                        sub[ind[j]] = sub[ind[j]].upper()
+                    else:
+                        sub[ind[j]] = sub[ind[j]].lower()
+            arr.append("".join(sub))
         return arr
