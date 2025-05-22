da3533b2bf83d2cccc4d432111b1b4500a42e565
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-05-22 20:38:22 +0300

    Time: 101 ms (31.28%) | Memory: 33.4 MB (36.41%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.py
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -5,11 +5,11 @@ class Solution:
         x = 0
         total = 0
         for i in range(len(nums)):
-            if nums[i] ^ x != total+nums[i]:
+            x ^= nums[i] 
+            total += nums[i]
+            if x != total:
                 x ^=nums[l]
                 total -= nums[l]
                 l +=1
-            x ^= nums[i] 
-            total += nums[i]
             maxm = max(maxm,i-l+1)
         return maxm
