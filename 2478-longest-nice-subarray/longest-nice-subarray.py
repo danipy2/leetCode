b11abf8cb3a188438d1abf0ad5c2e85d355af280
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-05-22 20:37:33 +0300

    Time: 107 ms (27.79%) | Memory: 31.8 MB (73.24%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.py
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -2,11 +2,14 @@ class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
         l = 0
         maxm = 1
-        x = nums[0]
-        for i in range(1,len(nums)):
-            while nums[i] & x:
+        x = 0
+        total = 0
+        for i in range(len(nums)):
+            if nums[i] ^ x != total+nums[i]:
                 x ^=nums[l]
+                total -= nums[l]
                 l +=1
-            x |= nums[i] 
+            x ^= nums[i] 
+            total += nums[i]
             maxm = max(maxm,i-l+1)
         return maxm
