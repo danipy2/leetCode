dac73733b06e69ede30c62db8ed03345b4284142
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-06-25 21:28:55 +0300

    Time: 39 ms (7.09%) | Memory: 18.8 MB (95.67%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,38 +1,33 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        size = len(isConnected)
-        self.root = [i for i in range(size)]
+        n = len(isConnected)
+        dsu = UnionFind(n)
+        numberOfComponents = n
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] and dsu.find(i) != dsu.find(j):
+                    numberOfComponents -= 1
+                    dsu.union(i, j)
+        return numberOfComponents
+class UnionFind:
+    def __init__(self, size):
+        # Write your code here
         self.rank = [0] * size
-        def find(x):
-            if x == self.root[x]:
-                return x
-            while x != self.root[self.root[x]]:
-                x  = self.root[self.root[x]]
+        self.root = [i for i in range(size)]
+    def find(self, x):
+        # Write your code here
+        if x == self.root[x]:
             return x
-        def union(x,y):
-            rootx = find(x)
-            rooty = find(y)
-            if  self.rank[rootx]!=self.rank[rooty]:
-                if  self.rank[rootx] >= self.rank[rooty]:
-                    for i in range(len(self.root)):
-                        if self.root[i] == rooty:
-                            self.root[i] = rootx
-                else:
-                    for i in range(len(self.root)):
-                        if self.root[i] == rootx:
-                            self.root[i] = rooty
+        return self.find(self.root[self.root[x]])
+    def union(self, x, y):
+        # Write your code here'
+        xp = self.find(x)
+        yp = self.find(y)
+        if xp!= yp:
+            if self.rank[xp] > self.root[yp]:
+                self.root[yp]= xp
+            elif self.rank[xp] < self.root[yp]:
+                self.root[xp] = yp
             else:
-                for i in range(len(self.root)):
-                    if self.root[i] == rooty:
-                        self.root[i] = rootx
-                self.rank[rootx] += 1
-        for i in range(len(isConnected)):
-            for j in range(i+1,len(isConnected[i])):
-                if isConnected[i][j]:
-                    union(i,j)
-        myset = set()
-        print(self.root)
-        for i in self.root:
-            myset.add(i)
-        return len(myset)
-    
+                self.root[xp] = yp
+                self.rank[yp] += 1
