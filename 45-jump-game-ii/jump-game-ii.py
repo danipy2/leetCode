3eb25e21d37b7fbf4c1f856cdbd6249909da528f
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-07-03 23:54:57 +0300

    Time: 7 ms (79.25%) | Memory: 18.4 MB (92.42%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -5,9 +5,7 @@ class Solution:
         t = 0 
         count = 0
         maxd = 0
-        while i+1 < len(nums):
-            if maxd >= len(nums)-1:
-                break
+        while maxd < len(nums)-1:
             while i <= maxd:
                 t = max(t,i+nums[i])
                 i+=1
