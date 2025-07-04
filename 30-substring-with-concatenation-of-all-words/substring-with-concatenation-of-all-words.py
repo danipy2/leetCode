5c4545a629943f6cc5b8b833483da68ae95ee401
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-07-04 14:25:06 +0300

    Time: 24 ms (80.73%) | Memory: 18.8 MB (38.37%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -1,50 +1,35 @@
-
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         if not s or not words:
             return []
-        
         word_count = Counter(words)
+        lgth = len(words[0])
         num_words = len(words)
-        L = len(words[0])
-        total_len = L * num_words
+        tlgth = lgth*num_words
         n = len(s)
         ans = []
-        
-        # Iterate over each alignment modulo L
-        for offset in range(L):
-            left = offset
+        for i in range(lgth):
+            left = i
             curr = defaultdict(int)
             count = 0
-            
-            # Slide right in steps of L
-            for right in range(offset, n - L + 1, L):
-                word = s[right:right + L]
-                # If it's one of our target words, include it
+            for j in range(i,n-lgth+1,lgth):
+                word = s[j:j+lgth]
                 if word in word_count:
-                    curr[word] += 1
-                    count += 1
-                    
-                    # If we've exceeded the allowed count,
-                    # move `left` forward until we're valid again
+                    curr[word] +=1
+                    count+=1
                     while curr[word] > word_count[word]:
-                        left_word = s[left:left + L]
+                        left_word = s[left:left + lgth]
                         curr[left_word] -= 1
-                        left += L
+                        left += lgth
                         count -= 1
-                    
-                    # If window size hits exactly num_words, record answer
                     if count == num_words:
                         ans.append(left)
-                        # Then slide left by one word to look for next possible
-                        left_word = s[left:left + L]
+                        left_word = s[left:left + lgth]
                         curr[left_word] -= 1
-                        left += L
+                        left += lgth
                         count -= 1
                 else:
-                    # Reset window if we see a non-matching word
                     curr.clear()
                     count = 0
-                    left = right + L
-        
+                    left = j + lgth
         return ans
