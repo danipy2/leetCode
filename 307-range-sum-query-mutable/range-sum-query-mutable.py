a8fe8a8fa2f8ca4a905dbb233d2bff034be0a26b
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-07-07 21:59:43 +0300

    Time: 557 ms (58.45%) | Memory: 36.4 MB (77.46%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/range-sum-query-mutable.py
@@ -1,13 +1,9 @@
 class NumArray:
     def __init__(self, nums: List[int]):
-        self.n = nums
         self.nums = [0 for i in range(len(nums))]
+        self.n = [0 for i in range(len(nums))]
         for i in range(len(nums)):
-            index = i+1
-            while index <= len(self.nums):
-                self.nums[index-1] += nums[i]
-                x = index & -index
-                index =index +x 
+            self.update(i,nums[i])
     def update(self, index: int, val: int) -> None:
         diff = val - self.n[index]
         self.n[index] = val
