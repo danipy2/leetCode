e6170529c803364ebc489081572b5695ae0a5a22
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-07-08 12:02:13 +0300

    Time: 2125 ms (5.08%) | Memory: 20.2 MB (6.25%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.py
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -1,40 +1,23 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
-        def query(n,ar):
-            l = n-1
-            r = len(arr)-1
-            t1 = 0
-            t2 = 0
-            t = 0
-            while l >0:
-                t1 += ar[l]
-                l -= l&-l
-            while n >0:
-                t += ar[n]
-                n -= n&-n
-            while r >0:
-                t2 += ar[r]
-                r -= r & -r
-            return [t1 ,t2-t]
-        def update(val,op,ar):
-            while val < len(ar):
-                ar[val]+=op
-                val += val & -val
-
-        arr = [0 for i in range(max(rating)+2)]
-        arr2 = [0 for i in range(max(rating)+2)]
-        arr3 = [0 for i in range(max(rating)+2)]
-        arr4= [0 for i in range(max(rating)+2)]
-        for i in rating:
-            l,r = query(i,arr)
-            arr2[i]= l
-            arr3[i] = r
-            update(i,1,arr)
-        count = 0
-        for i in range(len(rating)-1,-1,-1):
-            val = rating[i]
-            l ,r =  query(val,arr4)
-            count += l *arr3[val]
-            count += r * arr2[val]
-            update(val,1,arr4)
-        return count
+        cache = {}
+        def back(i,asc,count):
+            res = 0
+            if (i,asc,count) in cache:
+                return cache[(i,asc,count)]
+            if count == 3:
+                return 1
+            if i == len(rating):
+                return 0
+            for j in range(i+1,len(rating)):
+                if asc and rating[j] < rating[i]:
+                    res += back(j,asc,count+1)
+                elif not asc and rating[j] > rating[i]:
+                    res += back(j,asc,count+1)
+            cache[(i,asc,count)] = res
+            return res
+        res = 0
+        for i in range(len(rating)):
+            res +=back(i,True,1)
+            res +=back(i,False,1)
+        return res
