f5c6cb868944f744099ffb5897668a0d5be0521c
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-07-09 13:14:56 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (28.28%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,14 +1,15 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        myd = {}
-        count = 0
-        for i in answers:
-            if i==0 or i  not in  myd or myd[i]==0:
-                myd[i] = i
-                count+= i+1                           
-            else:
-                myd[i]-=1
-                
-                
-        return count
-               
+        c = Counter(answers)
+        total = 0
+        for n,val in c.items():
+            # if n == 0:
+            #     total += val
+            #     continue
+            num = n+1
+            # total += num
+            # if val-1 > n:
+            total += (((val-1)//(num)+1)) * num
+
+
+        return total
