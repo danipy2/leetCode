15aeac1b1b44a27a58b14f1fdda718a960c32abb
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-07-09 13:16:56 +0300

    Time: 2 ms (33.00%) | Memory: 17.8 MB (76.88%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -3,13 +3,6 @@ class Solution:
         c = Counter(answers)
         total = 0
         for n,val in c.items():
-            # if n == 0:
-            #     total += val
-            #     continue
             num = n+1
-            # total += num
-            # if val-1 > n:
-            total += (((val-1)//(num)+1)) * num
-
-
+            total += ceil(val/num) * num
         return total
