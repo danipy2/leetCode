a6366beec02525e4844f56a25dfef9b3f7871197
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-07-10 22:44:53 +0300

    Time: 39 ms (71.86%) | Memory: 20.5 MB (83.45%) - LeetSync

--- 475-heaters/heaters.py
+++ 475-heaters/heaters.py
@@ -1,15 +1,11 @@
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         heaters.sort()
-        maxm = 1000000009
         minm = 0
+        n = len(heaters)
         for i in houses:
-            ind = bisect_left(heaters,i)-1
-            leftlgth = maxm
-            rightlgth = maxm 
-            if 0<= ind<len(heaters):
-                leftlgth = i - heaters[ind]
-            if 0<= ind+1<len(heaters):
-                rightlgth = heaters[ind+1]- i
+            ind = bisect_left(heaters,i)
+            leftlgth = i - heaters[ind-1] if 0<= ind-1 <n else inf
+            rightlgth = heaters[ind]-i if 0<= ind<n else inf
             minm = max(minm,min(leftlgth,rightlgth))
         return minm
