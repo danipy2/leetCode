76c74e41cefe36918fe868a334a447fea41c46bf
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-07-27 23:55:55 +0300

    Time: 55 ms (75.58%) | Memory: 21 MB (81.32%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,38 +7,13 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        myset = set()
-        ans = [-1]
-        Found = False
-        def godeep(r,val):
-            nonlocal Found 
-            an = False
-            if not r or Found :
-                return False
-            if r.val == val:
-                
-                Found = True
-                if val in myset:
-                    ans[0] = r
-                    return False
-                myset.add(val)
-                return True
-            if r.val > val:
-                an |= godeep(r.left,val)
+        while root:
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
             else:
-                an |= godeep(r.right,val)
-            if an:
-                if r.val in myset:
-                    ans[0] = r
-                    Found = True
-                    return False
-                myset.add(r.val)
-                return True
-            return False
-        
-        godeep(root,p.val)
-        Found = False
-        godeep(root,q.val)
-        return ans[0]
+                return root
+
 
                 
