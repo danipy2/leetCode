a75ae56b821b64aa4d8d73ba042b8bfc77edfb93
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-08-01 23:39:47 +0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (15.84%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,16 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        arr1 = []
-        
-        def find(a,ans):
-            if not a:
-                arr1.append(ans)
-
+        arr = []
+        def find1(a,used):
+            if len(used) == len(nums):
+                arr.append(a.copy())
                 return 
-            for ind ,val in enumerate(a):
-                ans.append(val)
-                find(a[:ind]+a[ind+1:],ans.copy())
-                ans.pop()
-        find(nums,[])
-        return arr1
+            for ind ,val in enumerate(nums):
+                if val not in used:
+                    used.add(val)
+                    a.append(val)
+                    find1(a,used)
+                    a.remove(val)
+                    used.remove(val)
+                
+        find1([],set())
+        return arr
 
