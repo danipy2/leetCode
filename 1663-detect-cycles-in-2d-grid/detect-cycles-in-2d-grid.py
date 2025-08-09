2a55da28a208454600d15f0715b6ff4250adf628
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-08-09 21:07:52 +0300

    Time: 403 ms (65.98%) | Memory: 77.7 MB (84.19%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
@@ -1,30 +1,31 @@
 class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
         dirc = [(0,1),(1,0),(-1,0),(0,-1)]
-        visted = set()
         self.cond = False
-        self.c = False
         def dfs(x,y,p1,p2):
-            visted.add((x,y))
+            letter = grid[x][y]
             if self.cond:
                 return 
-
+            grid[x][y] = ""
+            
             for i,j in dirc:
                 nx = i+x
                 ny = j+y
                 
                 isbound = 0<=nx<len(grid) and 0<=ny<len(grid[0])
-                if isbound and grid[nx][ny] == grid[x][y]:
-                    v = (nx,ny) in visted 
-                    if v and (nx,ny) != (p1,p2):
-                            self.cond = True 
-                            return 
-                    if not v:
+                if isbound:
+                    if grid[nx][ny] == letter:
                         dfs(nx,ny,x,y)
+                    elif grid[nx][ny] ==""  and (nx,ny)!= (p1,p2):
+                        self.cond = True 
+                        break  
+            grid[x][y] = "*"
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if (i,j) not in visted:
+                if grid[i][j] != "*":
                     dfs(i,j,i,j)
+                    if self.cond:
+                        return True
         return self.cond
         
             
