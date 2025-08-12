7fc63b327d104925777cc6aee94d5cdd390b10d2
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-08-13 00:45:09 +0300

    Time: 72 ms (68.36%) | Memory: 33.9 MB (41.47%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -2,24 +2,30 @@ class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         dirc = [(1,0),(0,1),(-1,0),(0,-1)]
         lans = 0
+        w,h = len(grid),len(grid[0])
         def dfs(i,j):
-            ans = 1
             for x,y in dirc:
                 x += i
                 y += j
                 inbound = 0<=x<len(grid) and 0<=y<len(grid[0])
-                if not inbound:
-                    ans+= -25000
                 if inbound and grid[x][y]:
                     grid[x][y] = 0
-                    ans+= dfs(x,y)
-            return ans
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
+                    dfs(x,y)
+        for i in range(0,w,max(1,w-1)):
+            for j in range(h):
                 if grid[i][j]:
-                    grid[i][j] = 0 
-                    ans = dfs(i,j)
-                    lans += max(0,ans)
-        return lans
+                    grid[i][j] = 0
+                    dfs(i,j)
+        for i in range(0,h,max(h-1,1)):
+            for j in range(w):
+                if grid[j][i]:
+                    grid[j][i] = 0
+                    dfs(j,i)
+        count = 0
+        for i in range(len(grid)):
+            for j in grid[i]:
+                count+= j
+        return count
+        
 
         
