bec19733d0c26e1d73ce4787316a4ebf612246f7
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-08-13 01:59:48 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (44.11%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,12 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        arr = [0] * 26
-        arr1 = [0] * 26
-        sind = ord('a')
-        for i in s:
-            arr[ord(i)-sind] += 1
-        for i in t:
-            arr1[ord(i)-sind] += 1
-        for i in range(26):
-            if arr1[i] >arr[i]:
-                return chr(sind+i)
+        res = 0
+        for ch in s + t:
+            res ^= ord(ch)
+        return chr(res)
