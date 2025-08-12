beab522af8139cab064e18e2b3485ef55336e3a0
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-08-12 23:45:41 +0300

    Time: 48 ms (62.23%) | Memory: 34.5 MB (73.36%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,13 +1,15 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         ans = []
+        palindrome = set()
         def findpart(parts,arr):
             if not arr:
                 ans.append(parts.copy())
                 return 
             for i in range(len(arr)):
                 word = arr[0:i+1]
-                if word == word[::-1]:
+                if word in palindrome or word == word[::-1]:
+                    palindrome.add(word)
                     parts.append(arr[0:i+1])
                     findpart(parts,arr[i+1:])
                     parts.pop()
