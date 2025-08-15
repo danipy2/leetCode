1f483a1551267855ab75fa0cb8a20054ff289450
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-08-15 21:55:30 +0300

    Time: 1181 ms (31.25%) | Memory: 180.8 MB (5.19%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -3,27 +3,42 @@ class Solution:
         count = 1
         numb = 2
         found = False
-        def checkpunishment(res,rem):
+        memo  = {}
+        def checkpunishment(num,rem):
             nonlocal found
-            if found:
-                return 
             nonlocal numb
             nonlocal count
-            if not rem:
-                tot = sum(res)
-                if tot ==numb:
+            if found:
+                return True
+            if num<0:
+                return False
+            
+            if (num,rem) in memo:
+                if memo[(num,rem)]:
                     count += numb*numb
                     found = True
-                    return 
+                return memo[(num,rem)]
+            
+            
+            if not rem:
+                if not num:
+                    count += numb*numb
+                    found = True
+                    return True
+                return False
+            a = False
 
             for i in range(len(rem)):
-                res.append(int(rem[:i+1]))
-                checkpunishment(res,rem[i+1:])
-                res.pop()
+                val = int(rem[:i+1])
+                num-= val
+                a |=  checkpunishment(num,rem[i+1:])
+                num+= val
+            memo[(num,rem)] = a
+            return a
 
 
         for i in range(2,n+1):
             numb = i
             found = False
-            checkpunishment([],str(i*i))
+            checkpunishment(i,str(i*i))
         return count
