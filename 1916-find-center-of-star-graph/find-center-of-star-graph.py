95ff875411c07bc2205e5eae49ef5d843c4a56a1
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-08-31 18:01:24 +0300

    Time: 32 ms (33.36%) | Memory: 45.4 MB (90.46%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        arr = [0] * (len(edges)+2)
+        maxm = -1
+        ind = -1
+        for i,j in edges:
+            arr[i] +=1
+            arr[j] +=1
+            if arr[i]>maxm:
+                ind = i
+                maxm = arr[i]
+            if arr[j] > maxm:
+                ind = j
+                mam = arr[j]
+        return ind
+            
+        
