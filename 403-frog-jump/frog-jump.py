7518c24561d807101ad690008baaf3fb0769b787
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-09-25 19:54:34 +0300

    Time: 27 ms (98.28%) | Memory: 25.2 MB (44.20%) - LeetSync

--- 403-frog-jump/frog-jump.py
+++ 403-frog-jump/frog-jump.py
@@ -1,33 +1,24 @@
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
-        
         memo= set()
         c = {}
         for i in range(len(stones)):
             stone = stones[i]
             c[stone] = i
-        def dp(i,k):
+        def dp(stone,k):
             if k<=0:
                 return False
-            if i+1 >= len(stones):
+            if stone == stones[-1]:
                 return True
-            if (i,k) in memo:
+            if (stone,k) in memo:
                 return False
-            memo.add((i,k))
-            curr = stones[i]
-            n1 = curr+k+1
-            n2 = curr+k
-            n3 = curr+k-1
-            if n1 in c:
-                if dp(c[n1],k+1):
-                    return True
-            if n2 in c:
-                if dp(c[n2],k):
-                    return True
-            if n3 in c and dp(c[n3],k-1):
-                    return True
+            if stone not in c:
+                return False
+            memo.add((stone,k))
+            if dp(stone+k+1,k+1) or dp(stone+k,k) or dp(stone+k-1,k-1):
+                return True
             return False
         if stones[1] >1:
             return False
-        return dp(1,1)
+        return dp(stones[1],1)
 
