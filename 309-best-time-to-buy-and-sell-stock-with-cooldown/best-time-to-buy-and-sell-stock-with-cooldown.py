b89833157740079718698d36310f43ebcdc9cac0
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-10-03 19:30:54 +0300

    Time: 1303 ms (5.01%) | Memory: 18.3 MB (67.66%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,20 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo = [-1]*len(prices)
-
-        def dp(s):
-            if s>=len(memo):
-                return 0
-            if memo[s]==-1:
-                maxm = 0
-                for i in range(s+1,len(prices)):
-                    if prices[s]<prices[i]:
-                        diff = prices[i]-prices[s]
-                        maxm = max(maxm,diff+dp(i+2))
-                memo[s]  = max(maxm,dp(s+1))
-
-            return memo[s]
-
-        return dp(0)
+        table = [0]*len(prices)
+        for i in range(len(prices)-1,-1,-1):
+            maxm = table[i+1] if i != len(prices)-1 else 0 
+            for j in range(len(prices)-1,i,-1):
+                maxm = max(maxm,prices[j]-prices[i]+(table[j+2] if j+2<len(table) else 0))
+            table[i] = maxm
+        return table[0]
+            
             
             
