2bc3723ffd65fd04b1f2e83bb406981c9aa08245
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-10-21 16:16:41 +0300

    Time: 415 ms (12.65%) | Memory: 19.9 MB (33.32%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,12 +1,15 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        prv = [float("inf") ] * (n)
-        prv[k-1] = 0
+        prv = [float("inf") ] * (n+1)
+        prv[k] = 0
         for i in range(n-1):
-            curr = prv[:]
-            for u1,v1,w1 in times:
-                u,v,w = u1-1,v1-1,w1
-                curr[v] = min(curr[v],prv[u]+w)
-            prv = curr
-        maxm = max(prv)
+            cond = False
+            for u,v,w in times:
+                minm = min(prv[v],prv[u]+w)
+                if  minm != prv[v]:
+                    prv[v] = minm
+                    cond = True
+            if not cond:
+                break
+        maxm = max(prv[1:])
         return  maxm if maxm != float("inf") else -1
