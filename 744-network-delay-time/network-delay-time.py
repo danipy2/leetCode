3eb1df69b43dc5fd296ddca0f7b04db45873f3b6
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-10-21 15:31:11 +0300

    Time: 1054 ms (5.00%) | Memory: 20 MB (25.47%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,28 +1,12 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        d = {node:float("inf") for node in range(1,n+1)}
-        d[k] = 0
-        graph = {}
-        for start,dest,cost in times:
-            if start not in graph:
-                graph[start] = {}
-            graph[start][dest]=cost
-        qu = []
-        heapq.heappush(qu,(0,k))
-        maxm = 0
-        while qu:
-            cost,curr = heapq.heappop(qu)
-            if curr not in graph:
-                continue
-            for node in graph[curr]:
-                cost2 = graph[curr][node]
-                val = cost+cost2
-
-                if val < d[node]:
-                    d[node] = val
-                    heapq.heappush(qu,(val,node))
-        for key,value in d.items():
-            maxm = max(maxm,value)
-
-
-        return maxm if maxm!= float(inf) else -1
+        prv = [float("inf") ] * (n)
+        prv[k-1] = 0
+        for i in range(n-1):
+            curr = prv[:]
+            for u1,v1,w1 in times:
+                u,v,w = u1-1,v1-1,w1
+                curr[v] = min(curr[v],prv[u]+w)
+            prv = curr
+        maxm = max(prv)
+        return  maxm if maxm != float("inf") else -1
