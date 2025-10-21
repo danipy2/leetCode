dc9f7cde2eb57f306aa2dbbc647cc90e383e9414
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-10-21 09:46:07 +0300

    Time: 380 ms (59.72%) | Memory: 20.2 MB (9.08%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,29 +1,28 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        arr = [-1] * n
-        arr2 = [[] for i in range(n)]
-        for i in range(len(times)):
-            u,v,w = times[i]
-            arr2[u-1].append((w,v))
-
-        
-        heap = []
-        heapq.heappush(heap,(0,k))
-        count = 0
-        maxm = -1
-        while heap:
-            w,m= heapq.heappop(heap)
-            if arr[m-1]!=-1:
+        d = {node:float("inf") for node in range(1,n+1)}
+        d[k] = 0
+        graph = {}
+        for start,dest,cost in times:
+            if start not in graph:
+                graph[start] = {}
+            graph[start][dest]=cost
+        qu = []
+        heapq.heappush(qu,(0,k))
+        maxm = 0
+        while qu:
+            cost,curr = heapq.heappop(qu)
+            if curr not in graph:
                 continue
-            arr[m-1] = w
-            count+=1
-            maxm = max(maxm,w)
-            if count == n:
-                return maxm
-            
-            for i in range(len(arr2[m-1])):
-                w1,nod = arr2[m-1][i]
-                if arr[nod-1]==-1:
-                    heapq.heappush(heap,(w+w1,nod))
-        
-        return -1
+            for node in graph[curr]:
+                cost2 = graph[curr][node]
+                val = cost+cost2
+
+                if val < d[node]:
+                    d[node] = val
+                    heapq.heappush(qu,(val,node))
+        for key,value in d.items():
+            maxm = max(maxm,value)
+
+
+        return maxm if maxm!= float(inf) else -1
