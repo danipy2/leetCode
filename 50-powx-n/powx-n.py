bad8529bdb299d9dc9aec64e65d627964cd42ea9
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-10-30 22:41:08 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (11.27%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,3 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x,n)
+        num = abs(n)
+        total = 1
+        while num:
+            print(num,num&1)
+            if num & 1:
+                total *= x
+            x*=x
+            num = num >>1
+        if n<0:
+            return 1/total
+        return total
