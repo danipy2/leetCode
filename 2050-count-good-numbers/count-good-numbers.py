1c93924e43db8a53f70d5858dadcb32995ba8717
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-10-30 23:03:26 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (70.80%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,22 +1,14 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        
-        total = 1
-        p = (n+1)//2
-        x = 5
-        mod = 1000000007
-        while p:
-            if p&1:
-                total  = (total *x) % mod
-            x = (x*x) %mod
-            p=p>>1
-        p = (n)//2
-        x = 4
-        while p:
-            if p&1:
-                total  = (total *x) % mod
-            x = (x*x) % mod
-            p=p>>1
-        return total
+        def mult(x,p):
+            total = 1
+            mod = 1000000007
+            while p:
+                if p&1:
+                    total  = (total *x) % mod
+                x = (x*x)% mod
+                p=p>>1
+            return total
+        return (mult(5,(n+1)//2) * mult(4,n//2)) % 1000000007
         
                 
