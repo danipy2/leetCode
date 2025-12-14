6c5c393c16704a309057b0ed7e9fa0ecb7291a99
Daniel mola <165833791+danipy2@users.noreply.github.com>
2025-12-14 21:23:44 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.11%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,11 +3,12 @@ class Solution:
         ind = 0 
         ans = ""
         while True:
-            for i in range(1,len(strs)):
-                if ind>=len(strs[i-1]) or ind>= len(strs[i])  or strs[i][ind]!=strs[i-1][ind]:
-                    return ans
-            if ind>=len(strs[0]) or not strs:
+            if ind >= len(strs[0]):
                 return ans
+            letter = strs[0][ind]
+            for i in range(1,len(strs)):
+                if  ind>= len(strs[i])  or strs[i][ind]!=letter:
+                    return ans
             ans+=strs[0][ind]
             ind+=1
         
